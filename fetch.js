b0b0ccd01b897a2877fa6cb480162b2873478c29
yeisson099 <50432872+yeisson099@users.noreply.github.com>
2019-06-16 17:26:28 -0500

    COMMIT: fetch

---
+++ fetch.js
@@ -0,0 +1,21 @@
+'use strict'
+//fetch  y peticiones a servicios/apis rest
+//sustituto ajax
+var usuarios = [];
+var div = document.querySelector('#usuarios');
+var span = document.querySelector('.load');
+fetch('https://reqres.in/api/users')
+    .then(data => data.json())
+    .then(users =>{
+        usuarios = users.data;
+        console.log(usuarios);
+    
+    usuarios.map((user, i)=>{
+        var nombre = document.createElement('h2');
+        nombre.innerHTML = i+user.first_name+""+user.last_name;
+        div.appendChild(nombre);
+        
+    });
+    
+    span.style.display = "none";
+    });
