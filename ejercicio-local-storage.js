26671da803d344c94ca17f9da6a46d8384bcd236
yeisson099 <50432872+yeisson099@users.noreply.github.com>
2019-06-16 12:22:56 -0500

    COMMIT: ejercicio local storage bien

--- ejercicio-local-storage.js
+++ ejercicio-local-storage.js
@@ -1,30 +1,34 @@
 'use strict'
 var formulario = document.querySelector('#form');
-var boton = document.querySelector("#eliminar");
-var borrar = document.querySelector("#elim");
-var div = document.querySelector("#peliculas");
+// var boton = document.querySelector("#eliminar");
+// var borrar = document.querySelector("#elim");
 formulario.addEventListener('submit',function(){
-    // event.preventDefault();
+    
+    event.preventDefault();
     var input = document.querySelector('#pelicula').value;
-
         if(input.length >= 1 ){
         localStorage.setItem(input,input); 
     }
+    var ul = document.querySelector("#peliculas");
     for (var i in localStorage) {
         if(typeof localStorage[i] == 'string'){
-            var li = document.createElement('li');
-            li.append(localStorage[i]);
-            div.append(li)
-            
+            // if(localStorage.getItem(input) == localStorage.getItem(input)){
+                var li = document.createElement("li");
+                li.append(localStorage.getItem(input));
+                ul.append(li);
+            // }
+
+            break;
         }
      
     }
     this.reset();
-});
-boton.addEventListener("click", function () {
-    localStorage.clear();
     
 });
+// boton.addEventListener("click", function () {
+//     localStorage.clear();
+    
+// });
 // borrar.addEventListener("click", function () {
 //     div.remove
 // });
